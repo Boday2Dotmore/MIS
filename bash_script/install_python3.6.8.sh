f99058d21b9e5bf5f7fe3252a549b2c709150f27
Boday2Dotmore <boday@dotmore.com.tw>
2019-03-06 10:14:00 +0800

    Create install_python3.6.8.sh

---
+++ bash_script/install_python3.6.8.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+yum -y install gcc openssl-devel bzip2-devel libffi-devel
+cd /tmp
+curl -O https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tgz
+tar zxvf Python-3.6.8.tgz
+cd Python-3.6.8
+./configure --enable-optimizations --with-ssl && make altinstall
+rm -rf /tmp/Python*
+
+
+tee -a ~/.zshrc <<EOM
+alias python="/usr/local/bin/python3.6"
+alias pip="/usr/local/bin/pip3.6"
+EOM
+source ~/.zshrc
+
+echo "Python-3.6.8 Install Done!!"
